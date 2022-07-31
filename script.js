f7fbb78900b1b9a15788fd26178003df8418ec17
Oleksandr Nesterenko <stan0men@yahoo.com>
2022-07-31 20:47:57 +0300

    add expirience section

--- script.js
+++ script.js
@@ -2,24 +2,21 @@ const button = document.querySelector('.btn');
 
 const displayButton = () => {
   window.addEventListener('scroll', () => {
-    console.log(window.scrollY);
-  
     if (window.scrollY > 100) {
-      button.style.display = "block";
+      button.style.display = 'block';
     } else {
-      button.style.display = "none";
+      button.style.display = 'none';
     }
   });
 };
 
 const scrollToTop = () => {
-  button.addEventListener("click", () => {
+  button.addEventListener('click', () => {
     window.scroll({
       top: 0,
       left: 0,
-      behavior: 'smooth'
-    }); 
-    console.log(event);
+      behavior: 'smooth',
+    });
   });
 };
 
