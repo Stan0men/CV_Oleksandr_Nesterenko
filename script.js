c763b0101ce5f6db9e116f7c07676146597292d6
Stan0men <stan0men@yahoo.com>
2023-06-21 16:06:19 +0300

    add back-to-top button

--- script.js
+++ script.js
@@ -1,24 +1,16 @@
-const button = document.querySelector('.btn');
+let backToTopButton = document.querySelector(".back-to-top");
 
-const displayButton = () => {
-  window.addEventListener('scroll', () => {
-    if (window.scrollY > 100) {
-      button.style.display = 'block';
-    } else {
-      button.style.display = 'none';
-    }
-  });
+window.onscroll = function () {
+  scrollFunction();
 };
 
-const scrollToTop = () => {
-  button.addEventListener('click', () => {
-    window.scroll({
-      top: 0,
-      left: 0,
-      behavior: 'smooth',
-    });
-  });
-};
-
-displayButton();
-scrollToTop();
+function scrollFunction() {
+  if (
+    document.body.scrollTop > 400 ||
+    document.documentElement.scrollTop > 400
+  ) {
+    backToTopButton.style.display = "block";
+  } else {
+    backToTopButton.style.display = "none";
+  }
+}
