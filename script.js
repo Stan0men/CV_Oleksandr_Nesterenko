ff717888df0c1d3807c64f8f945be1041f422b8e
Stan0men <stan0men@yahoo.com>
2023-06-22 16:32:29 +0300

    add mobile version

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
+//hamburger menu
+
+window.onload = function () {
+  var menuToggle = document.querySelector(".menu-toggle");
+  var menu = document.querySelector(".menu");
+
+  menuToggle.addEventListener("click", function () {
+    this.classList.toggle("open");
+    menu.classList.toggle("open");
+  });
+};
+
+//back to top button
+
 let backToTopButton = document.querySelector(".back-to-top");
 
 window.onscroll = function () {
