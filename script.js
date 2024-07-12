799dd26c37656eb2ad05b7c7e0105380eb76fae8
stan0men@yahoo.com <stan0men@yahoo.com>
2024-07-12 20:05:16 +0300

    Add rotating circle animation

--- script.js
+++ script.js
@@ -1,25 +1,22 @@
 //hamburger menu
 
 function toggleMenu() {
-  var menu = document.querySelector(".menu");
-  menu.classList.toggle("show");
+  var menu = document.querySelector('.menu');
+  menu.classList.toggle('show');
 }
 
 //back to top button
 
-let backToTopButton = document.querySelector(".back-to-top");
+let backToTopButton = document.querySelector('.back-to-top');
 
 window.onscroll = function () {
   scrollFunction();
 };
 
 function scrollFunction() {
-  if (
-    document.body.scrollTop > 400 ||
-    document.documentElement.scrollTop > 400
-  ) {
-    backToTopButton.style.display = "block";
+  if (document.body.scrollTop > 400 || document.documentElement.scrollTop > 400) {
+    backToTopButton.style.display = 'block';
   } else {
-    backToTopButton.style.display = "none";
+    backToTopButton.style.display = 'none';
   }
 }
